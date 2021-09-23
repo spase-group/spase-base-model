2bda537b686e0715828bc2a0b224cd91e8a6e59b
Todd King <tking@igpp.ucla.edu>
2021-09-23 11:17:40 -0700

    Revisions to ProcessingLevel and related items.

--- build-all.sh
+++ build-all.sh
@@ -5,6 +5,8 @@ versions=(${1:-})
 suffix=${2:-}
 prefix=${3:-spase-base}
 
+draft=()
+
 # Set short name
 shortname=""
 if [ "$prefix" = "spase-base" ]
@@ -21,7 +23,11 @@ fi
 if [ -z "$versions" ]
 then
 versions=(1.1.0 1.2.0 1.2.1 1.2.2 1.3.0 2.0.0 2.1.0 2.2.0 2.2.1 2.2.2 2.2.4 2.2.6 2.2.8 2.2.9 2.3.0 2.3.1 2.3.2 2.4.0)
+draft=()
 fi
+
+# Create "build" folder if it doesn't exist
+mkdir -p build
 
 # If suffix specified, add dash to beginning. If a dash (-) do not add suffix
 if [ -n "$suffix" ]
@@ -34,12 +40,13 @@ then
    fi
 fi
 
+# All release versions or specific one from comment-line
 for ver in "${versions[@]}"
 do
 	echo ${ver}${suffix}
 	node ../../spase-model-tools-node/src/makejson.js -b ${prefix}-${ver} -o ../build/${prefix}-${ver}${suffix}.json 
 	node ../../spase-model-tools-node/src/makexsd.js -d build/${prefix}-${ver}${suffix}.json -o build/${prefix}-${ver}${suffix}.xsd
-	node ../../spase-model-tools-node/src/makepdf.js -b ${prefix}-${ver}/template -d ../../build//${prefix}-${ver}${suffix}.json -o ../../build/${prefix}-${ver}${suffix}.pdf
+	node ../../spase-model-tools-node/src/makepdf.js -b ${prefix}-${ver}/template -d ../../build/${prefix}-${ver}${suffix}.json -o ../../build/${prefix}-${ver}${suffix}.pdf
 	if [ -n "$shortname" ]	
 	then
 	   cp build/${prefix}-${ver}${suffix}.json build/${shortname}-${ver}${suffix}.json
@@ -49,3 +56,22 @@ do
 	   cp build/${prefix}-${ver}${suffix}.xsd build/${shortname}-${ver//\./_}${suffix}.xsd
 	fi
 done
+
+# All draft versions
+suffix="-draft"
+for ver in "${draft[@]}"
+do
+	echo ${ver}${suffix}
+	node ../../spase-model-tools-node/src/makejson.js -b ${prefix}-${ver} -o ../build/${prefix}-${ver}${suffix}.json 
+	node ../../spase-model-tools-node/src/makexsd.js -d build/${prefix}-${ver}${suffix}.json -o build/${prefix}-${ver}${suffix}.xsd
+	node ../../spase-model-tools-node/src/makepdf.js -b ${prefix}-${ver}/template -d ../../build/${prefix}-${ver}${suffix}.json -o ../../build/${prefix}-${ver}${suffix}.pdf
+	if [ -n "$shortname" ]	
+	then
+	   cp build/${prefix}-${ver}${suffix}.json build/${shortname}-${ver}${suffix}.json
+	   cp build/${prefix}-${ver}${suffix}.xsd build/${shortname}-${ver}${suffix}.xsd
+	   cp build/${prefix}-${ver}${suffix}.pdf build/${shortname}-${ver}${suffix}.pdf
+	   
+	   cp build/${prefix}-${ver}${suffix}.xsd build/${shortname}-${ver//\./_}${suffix}.xsd
+	fi
+done
+
