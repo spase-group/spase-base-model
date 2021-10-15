e381b2285ca87414b6d2631d065f0eeb6aeae8c2
Todd King <tking@igpp.ucla.edu>
2021-10-14 17:05:15 -0700

    Add 2.4.1 draft and update image in 2.4.0 documentation.

--- build-all.sh
+++ build-all.sh
@@ -23,7 +23,7 @@ fi
 if [ -z "$versions" ]
 then
 versions=(1.1.0 1.2.0 1.2.1 1.2.2 1.3.0 2.0.0 2.1.0 2.2.0 2.2.1 2.2.2 2.2.4 2.2.6 2.2.8 2.2.9 2.3.0 2.3.1 2.3.2 2.4.0)
-draft=()
+draft=(2.4.1)
 fi
 
 # Create "build" folder if it doesn't exist
