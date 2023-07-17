1fe9f607b8752ad756b9f98b8388db0f41b3fea5
Lee Frost Bargatze <lfb@igpp.ucla.edu>
2023-07-16 20:47:24 -0700

    Ingest newest updates of the SPASE Base Model draft version 2.6.0 tab files, etc., LFB 20230716

--- spase-base-2.6.0/template/helpers/util.js
+++ spase-base-2.6.0/template/helpers/util.js
@@ -1,89 +1,125 @@
-// Convert a object into an array
-function array(obj, name) {
-	if(! obj) { console.log("array(): Object is not defined."); return []; }
-	if(! obj[name]) { console.log("array(): Element with name '" + name +"' is not defined."); return []; }
-	
-	return obj[name];
+// Convert an object into an array
+
+function array(obj,name) {
+
+   if(! obj) { console.log("array():Object is not defined."); return[]; }
+   if(! obj[name]) { console.log("array(): Element with name '"+name+"' is not defined.");return[]; }
+   return obj[name];
+
 }
 
 // Group elements of an array by common element value.
-function group(obj, name) {
-	if(! obj) { console.log("group(): Object is not defined."); return []; }
-	
-	var results = [];
-	var set = { title: "", entry: []};
-	
-	for(let i = 0; i < obj.length; i++) {
-		var item = obj[i];
-		if(item[name] != set.title) {
-			if(set.title.length > 0) results.push(set);
-			set = new Object();
-			set.title = item[name];
-			set.entry = [];
-		}
-		set.entry.push(item);
-	}
-	if(set.title.length > 0) results.push(set);
-	
-	return results;
+
+function group(obj,name) {
+
+   if(! obj) { console.log("group(): Object is not defined."); return []; }
+   var results = [];
+   var set = { title:"",entry:[]};
+
+   for (let i=0;i<obj.length;i++) {
+
+      var item = obj[i];
+
+      if(item[name] != set.title) {
+
+         if(set.title.length > 0) results.push(set);
+         set = new Object();
+         set.title = item[name];
+         set.entry = [];
+
+      }
+
+      set.entry.push(item);
+   }
+
+   if(set.title.length > 0) results.push(set);
+
+   return results;
 }
 
 // Build a tree of nested arrays
-function buildTreeArray(ont, name, occurrence) {
-	var item = { name: name, occurrence: occurrence, children: [] };
-	if(occurrence == null) {	// Root of tree
-		item.occurrence = "1";
-	}
-	
-	if(ont[name]) {	// Build up children
-		var list = ont[name];
-		var keys = Object.keys(list);
-		if(keys.length > 0) {
-			for (var i = 0; i < keys.length; i++) {
-				var key = keys[i];
-				item.children.push( buildTreeArray(ont, key, list[key].occurrence) );
-			}
-		}
-	}
-	
-	if(occurrence == null) {	// Root of tree
-		return [ item ];
-	}
-	
-	return item;	// part of tree
+
+function buildTreeArray(ont,name,occurrence) {
+
+   var item = { name:name,occurrence:occurrence,children:[] };
+
+   if(occurrence == null) { // Root of tree
+      item.occurrence = "1";
+
+   }
+
+   if(ont[name]) { // Build up children
+
+      var list = ont[name];
+      var keys = Object.keys(list);
+
+      if(keys.length > 0) {
+
+         for (var i = 0; i < keys.length; i++) {
+
+            var key = keys[i];
+            item.children.push( buildTreeArray(ont,key,list[key].occurrence) );
+
+         }
+
+      }
+
+   }
+
+   if(occurrence == null) { // Root of tree
+      return [item];
+   }
+
+   return item; // part of tree
+
 }
 
 // Build a tree of nested arrays
-function buildTree(ont, name, occurrence, prefix) {
-	var set = [];
-	set.push( { prefix: prefix, name: name, occurrence: occurrence } );
-	
-	if(ont[name]) {	// Build up children
-		var list = ont[name];
-		var keys = Object.keys(list);
-		if(keys.length > 0) {
-			for (var i = 0; i < keys.length; i++) {
-				var key = keys[i];
-				set.push( ...buildTree(ont, key, list[key].occurrence, "|        " + prefix) );
-			}
-		}
-	}
-	
-	return set;
+
+function buildTree(ont,name,occurrence,prefix) {
+
+   var set = [];
+   set.push( { prefix:prefix,name:name,occurrence:occurrence } );
+
+   if(ont[name]) { // Build up children
+
+      var list = ont[name];
+      var keys = Object.keys(list);
+
+      if(keys.length > 0) {
+
+         for (var i = 0; i < keys.length; i++) {
+
+            var key = keys[i];
+            set.push( ...buildTree(ont,key,list[key].occurrence,"|        "+prefix) );
+
+         }
+
+      }
+
+   }
+
+   return set;
+
 }
 
 function now() {
-	var today = new Date();
-	var dd = String(today.getDate()).padStart(2, '0');
-	var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-	var yyyy = today.getFullYear();
 
-	today = yyyy + '-' + mm + '-' + dd;
-	return(today);
+   var today=new Date();
+   var dd=String(today.getDate()).padStart(2,'0');
+   var mm=String(today.getMonth()+1).padStart(2,'0'); //January is 0!
+   var yyyy=today.getFullYear();
+
+   today=yyyy+'-'+mm+'-'+dd;
+
+   return(today);
+
 }
 
 function dump(obj) {
-	console.log("***");
- 	console.log(JSON.stringify(obj, null, 3));
-	console.log("***");
- }
+
+   console.log("***");
+   console.log(JSON.stringify(obj,null,3));
+   console.log("***");
+
+}
--- spase-base-2.6.0/template/scripts/scripts.js
+++ spase-base-2.6.0/template/scripts/scripts.js
@@ -1,67 +1,72 @@
-const fs = require('fs');
-const path = require('path');
-const jsreport = require('jsreport-proxy');
-const loader = require('loader.js');
+const fs=require('fs');
+const path=require('path');
+const jsreport=require('jsreport-proxy');
+const loader=require('loader.js');
 
-function beforeRender (req, res) {
-    // req.data = req.data || {}
-	console.log("Before render");
+function beforeRender (req,res) {
+
+   // req.data=req.data || {}
+
+   console.log("Before render");
 }
 
-async function afterRender(req, res) {
-	const toc = loader.loadFile(req.data.options.toc, "utf8");
-	const coverPage = loader.loadFile(req.data.options.cover, "utf8");
-	const helpers = loader.loadAll(req.data.options.helpers, ".js"); 
-	/*
-	const toc = loader.loadFile(path.join(req.data.options.base, req.data.options.toc), "utf8");
-	const coverPage = loader.loadFile(path.join(req.data.options.base, req.data.options.cover), "utf8");
-	const helpers = loader.loadAll(path.join(req.data.options.base, req.data.options.helpers), ".js"); 
-	*/
-	
-	console.log("afterRender(): initialize");
-	
-    // after rendering with some pdf recipe we can use .parse to read the text in the pdf
-    let $pdf = await jsreport.pdfUtils.parse(res.content, true)
+async function afterRender(req,res) {
+   const toc=loader.loadFile(req.data.options.toc,"utf8");
+   const coverPage=loader.loadFile(req.data.options.cover,"utf8");
+   const helpers=loader.loadAll(req.data.options.helpers,".js");
+   /*
+   const toc=loader.loadFile(path.join(req.data.options.base,req.data.options.toc),"utf8");
+   const coverPage=loader.loadFile(path.join(req.data.options.base,req.data.options.cover),"utf8");
+   const helpers=loader.loadAll(path.join(req.data.options.base,req.data.options.helpers),".js");
+   */
 
-    // Add a table of contents (if requested)
-	if(toc.length > 0) {
-		console.log("Adding table of contents ...");
-		if ($pdf.pages[0] && $pdf.pages[0].text != null) {
-			const appendRes = await jsreport.render({
-				template: {
-					content: toc,
-					engine: 'handlebars',
-					recipe: 'chrome-pdf',
-					helpers: helpers,
-				},
-				data: {
-					...req.data,
-					$pdf
-				}
-			});
+   console.log("afterRender():initialize");
 
-			res.content = await jsreport.pdfUtils.prepend(res.content, appendRes.content);
-		}
-	}
+   // after rendering with some pdf recipe we can use .parse to read the text in the pdf
 
-	if(coverPage.length > 0) {
-		console.log("Adding cover page...");
-		if ($pdf.pages[0] && $pdf.pages[0].text != null) {
-			const coverRender = await jsreport.render({
-				template: {
-					content: coverPage,
-					engine: 'handlebars',
-					recipe: 'chrome-pdf',
-					helpers: helpers
-				},
-				data: {
-					...req.data,
-					$pdf
-				}
-			});
+   let $pdf=await jsreport.pdfUtils.parse(res.content,true)
 
-			// finally add a cover page
-			res.content = await jsreport.pdfUtils.prepend(res.content, coverRender.content)
-		}
-	}
+   // Add a table of contents (if requested)
+
+   if(toc.length > 0) {
+      console.log("Adding table of contents ...");
+      if ($pdf.pages[0] && $pdf.pages[0].text != null) {
+         const appendRes=await jsreport.render({
+            template:{
+               content:toc,
+               engine:'handlebars',
+               recipe:'chrome-pdf',
+               helpers:helpers,
+            },
+            data:{
+               ...req.data,
+               $pdf
+            }
+         });
+
+         res.content=await jsreport.pdfUtils.prepend(res.content,appendRes.content);
+      }
+   }
+
+   if(coverPage.length > 0) {
+      console.log("Adding cover page ...");
+      if ($pdf.pages[0] && $pdf.pages[0].text != null) {
+         const coverRender=await jsreport.render({
+            template:{
+               content:coverPage,
+               engine:'handlebars',
+               recipe:'chrome-pdf',
+               helpers:helpers
+            },
+            data:{
+               ...req.data,
+               $pdf
+            }
+         });
+
+         // finally add a cover page
+
+         res.content=await jsreport.pdfUtils.prepend(res.content,coverRender.content)
+      }
+   }
 }
